657e729405b5b9536fe7bf78764f1a1076f4a6bc
Franco Colmenarez <info@francoacg.com>
2022-02-07 18:43:52 -0500

    Refactor (?)

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,11 @@
 #[macro_export]
 macro_rules! box_array {
     ($val:expr ; $len:expr) => {{
-        // Use a generic function so that the pointer cast remains type-safe
         fn vec_to_boxed_array<T>(vec: Vec<T>) -> Box<[T; $len]> {
             let boxed_slice = vec.into_boxed_slice();
-
             let ptr = ::std::boxed::Box::into_raw(boxed_slice) as *mut [T; $len];
-
             unsafe { Box::from_raw(ptr) }
         }
-
         vec_to_boxed_array(vec![$val; $len])
     }};
 }
