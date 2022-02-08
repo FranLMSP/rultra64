4cfae27629a9598afc47cc8bb4dfc90e9e35c3af
Kim-Dewelski <edwin.ljung@gmail.com>
2022-02-08 03:06:50 +0100

    Removed dumb thing

--- bin/main.rs
+++
@@ -1,7 +0,0 @@
-use rultra64::gui::EmulatorApp;
-
-fn main() {
-    let app = EmulatorApp::default();
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(Box::new(app), native_options);
-}
